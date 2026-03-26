ab7e794e5f14862753ee36b23ac79b630a0236bd
sagemyrage <shtrixix@gmail.com>
2026-03-26 12:24:52 +0300

    add comments

--- lib.go
+++ lib.go
@@ -1,9 +1,11 @@
 package workspace_lib
 
+// AddNums принимает два числа int и возвращает их сумму
 func AddNums(a, b int) int {
 	return a + b
 }
 
+// SubNums принимает два числа int и возвращает их разность
 func SubNums(a, b int) int {
 	return a - b
 }
